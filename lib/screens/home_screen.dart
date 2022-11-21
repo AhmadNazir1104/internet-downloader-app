1788364a1773e1ddbb2ecf55afb0c4e45fc0ad33
zafar-iqbal1 <57129690+zafar-iqbal1@users.noreply.github.com>
2022-11-21 18:50:29 +0500

    Changing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:internet_speed/pages/history_page.dart';
-import 'package:internet_speed/pages/monitor_page.dart';
-import 'package:internet_speed/pages/settings_page.dart';
-import 'package:internet_speed/pages/tool_page.dart';
-
-import 'package:internet_speed/widgets/listitems.dart';
-
-import '../pages/speed_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,69 +6,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Appp shbfhdh')),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                ListItems(
-                    imgPath: "assets/images/logo.png",
-                    title: "speed",
-                    onTap: (() {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return SpeedPage();
-                        },
-                      ));
-                    })),
-              ],
-            ),
-            Column(
-              children: [
-                ListItems(
-                    imgPath: "assets/images/logo.png",
-                    title: "Monitor",
-                    onTap: (() {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return MonitorPage();
-                      }));
-                    })),
-              ],
-            ),
-            Column(
-              children: [
-                ListItems(
-                    imgPath: "assets/images/logo.png",
-                    title: "Tools",
-                    onTap: (() {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return ToolsPage();
-                        },
-                      ));
-                    })),
-              ],
-            ),
-            Column(
-              children: [
-                ListItems(
-                    imgPath: "assets/images/logo.png",
-                    title: "setting",
-                    onTap: (() {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return settingPage();
-                        },
-                      ));
-                    })),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+        child: Scaffold(
+            appBar: AppBar(title: Text('Appp shbfhdh')),
+            body: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [])));
   }
 }
--- lib/utility/app_strings.dart
+++ lib/utility/app_strings.dart
@@ -1,12 +1,1 @@
-class appString {
-  String monitorFirstTxt =
-      "Ping Monitor enables you to monitor your\n Internet connection to detect outages and\n connection problems";
-  String monitorSecondTxt =
-      "Monitors can be set up to specific\n network and or locations, depending on\n your needs";
-  String monitorThirdTxt =
-      "After Setting up a monitor, pings are \n recorded at regular intervals";
-  String monitorFourthTxt =
-      "Pings allow you to check your internet \n connection while using the least amount of \n data possible";
-  String monitorFifthTxt =
-      "To set up a monitor, connect to the desired\n network and click on the + button below";
-}
+class appString {}
